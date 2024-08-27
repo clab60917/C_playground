12f2b644a382e377a3825a7d0aa5e50ec3dc8ae6
Clément <clab60917@eleve.isep.fr>
2024-08-27 15:32:07 +0200

    learning in progress

--- four.c
+++ four.c
@@ -4,21 +4,23 @@
 int main()
 {
     int password;
-
-    printf("Veuillez entrer le mot de passe maxi secret : %s\n");
+    char name[50]; 
+    printf("Quel est votre nom ? Ecrire ici : ");
+    scanf("%s", name); // ATTENTION, SCANF NE SUPPORTE PAS /N
+    printf("Veuillez entrer le mot de passe maxi secret, %s : ", name);
     scanf("%d", &password);
 
     if (password == 987)
         
     {
-        printf("Bienvenue monsieur %d");
+        printf("Bienvenue monsieur %s\n", name); // METTRE EN DERNIERE LIGNE ÇA POUR PAS AVOIR LE %
         FILE *fichier = fopen("fichiertest1.txt", "w");
-        fputs("Voici la note secrete agent : recupere l'oiseau dans le nid. Ce message s'autodetruira. Over. %s\n", fichier);
+        fputs("Voici la note secrete agent : recupere l'oiseau dans le nid. Ce message s'autodetruira. Over. \n", fichier);
         fclose(fichier);
     }
     else
     {
-        printf("Vous ne pouvez pas acceder à ce document confidentiel %s\n");
+        printf("Vous ne pouvez pas acceder à ce document confidentiel \n");
     }
 
     return 0;
