bdf0bac3f436b6076158aefedbc236919189602f
Clément <clab60917@eleve.isep.fr>
2024-08-27 14:56:42 +0200

    learning in progress

--- second.c
+++ second.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    int nombres[5] = {1,2,3,4,5}; // la je fais juste un tableau de 5 nombres entiers
-    printf("%d", nombres[4]);
-
+    int nombres[10] = {13, 25, 343, 41, 512, 602, 79, 238, 909, 10}; // la je fais juste un tableau de 5 nombres entiers
+    printf("%i", nombres[2]); // ATTENTION -> Ici index[0] = 13 et index[1] = 25
+    // EN C LE COMPTAGE COMMENCE A 0 !!
 
 
     return 0;
---
+++ third.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    struct Chien { // Ici on definit une structure
+        int age;
+        char name[50];
+        char race[50];
+    };
+
+    struct Chien Vico; // Ici on utilise la structure
+
+        // stpcpyc parce que c'est des chaines de caracteres et impossible de race = Bichon
+        stpcpy(Vico.name, "Vico"); // strcpy(Vico.name, "Vico"); copie "Vico" dans le tableau de caractères Vico.name.
+        stpcpy(Vico.race, "Bichon");
+
+        // Vu que c'est un int on a le droit de faire age = 1
+        Vico.age = 1;
+
+        // Print des valeurs renseignées
+        printf("%s\n", Vico.name);
+        printf("%d\n", Vico.age);
+
+        printf("%s\n", Vico.race);
+
+
+    return 0;
+}
