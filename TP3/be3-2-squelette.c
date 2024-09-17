dd0178659d14b182a2d32b1400ffef4005d2317f
Clément <clab60917@eleve.isep.fr>
2024-09-17 16:12:24 +0200

    continue

--- TP3/be3-2-squelette.c
+++ TP3/be3-2-squelette.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct arbre {
-  int x;
-  struct arbre* fg;
-  struct arbre* fd;
+typedef struct arbre
+{
+  int node;
+  struct arbre *fg;
+  struct arbre *fd;
 } A;
-int displayArbre(){
-  if (arbre == NULL){
-    return 0;
-  }
 
+// QUESTION 5 : on veut afficher l'arbre et la valeur de chaque noeud + adresse de chaque noeud
+void displayArbre(A *node)
+{
+  if (node == NULL)
+  {
+    printf("Arbre vide\n");
+    return 1;
+  }
+  printf("Node : %d\n", node->node);
+  printf("Adresse : %p\n", node);
 }
 
-int main(){
-  A d = { .x = 5, .fg = NULL, .fd = NULL };
-  A c = { .x = 50, .fg = NULL, .fd = NULL };
-  A b = { .x = 1, .fg = NULL, .fd = &d };
-  A a = { .x = 30, .fg = &b, .fd = &c };
+int main()
+{
+  A d = {.node = 5, .fg = NULL, .fd = NULL};
+  A c = {.node = 50, .fg = NULL, .fd = NULL};
+  A b = {.node = 1, .fg = NULL, .fd = &d};
+  A a = {.node = 30, .fg = &b, .fd = &c};
+  displayArbre(&a);
 
   return 0;
 }
